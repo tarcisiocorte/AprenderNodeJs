1ec4f895987d35f0f5d38f0164d7ca93e19bedf1
tarcisiocorte <tarcisiocorte@gmail.com>
2014-12-01 15:30:22 -0200

    Correção para modificar aspas

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var http = require('http'); 
-var server = http.createServer(function (req, res)
-	{ 
+var server = http.createServer(function (req, res){ 
+		
 		res.writeHead(200, {'Content-Type': 'text/html'}); 
-		res.write(“<h1>Seja bem-vindo!</h1>”); 
+		res.write("<h1>Seja bem-vindo!</h1>"); 
 		res.end(); 
 	}); 
 server.listen(1337);
