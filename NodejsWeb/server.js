7e80f9a1470c34cc950cc45fc1540ee631e26fe7
tarcisiocorte <tarcisiocorte@gmail.com>
2014-12-04 17:39:50 -0200

    alterado arquivo para testar no browser

--- NodejsWeb/server.js
+++ NodejsWeb/server.js
@@ -1,6 +1,6 @@
 ﻿var http = require('http');
-var port = process.env.port || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+    res.end('Hello Node.JS!');
+}).listen(8080);
+console.log('Server running at http://localhost:8080/');
