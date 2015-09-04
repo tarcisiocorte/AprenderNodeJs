eed7e38829f529cdf27b56eef670d11ffec63823
tarcisiocorte@gmail.com <tarcisiocorte@gmail.com>
2015-09-04 11:02:56 +0100

    Added Mongodb....and tested a connection

---
+++ app/routes.js
@@ -0,0 +1,20 @@
+var User = require('./models/user');
+module.exports = function(app){
+	app.get('/', function(req, res){
+		res.send("Hello world");
+	});
+
+	app.get('/:username/:password', function(req, res){
+		var newUser = new User();
+		newUser.local.username = req.params.username;
+		newUser.local.password = req.params.password;
+		console.log(newUser.local.username + " " + newUser.local.password);
+		newUser.save(function(err){
+			if(err)
+				throw err;
+		});
+		console.log(newUser.local.username);
+		console.log(newUser.local.username);
+		res.send("Success!");
+	})
+}
